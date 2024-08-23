4ed2f426b6729abc19e493e3d6683690dc547c16
lucasonazes <lucasonazes@gmail.com>
2024-08-23 08:55:48 -0300

    starting react hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,13 +1,91 @@
 import './styles.css';
 
-import { Component } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? 
+  allPosts.filter(post => {
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  })
+  : 
+  posts;
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, [])
+
+  useEffect(() => {
+    console.log(new Date().toLocaleDateString('pt-br'));
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+  }
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  }
+
+  return (
+    <section className='container'>
+      <div className='search-container'>
+        {!!searchValue && (
+          <h1>Searching for: {searchValue}</h1>
+        )}
+
+        <TextInput searchValue={searchValue} handleChange={handleChange}/>
+      </div>
+
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>Post not found =(</p>
+      )}
+
+      <div className='button-container'>
+        {!searchValue && (
+          <Button
+            text='Load more posts' 
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+  );
+}
+
+//
+// Same component, but with class
+//
+
+/*
+export class Home2 extends Component {
 
   state = {
     posts: [],
@@ -88,3 +166,4 @@ export class Home extends Component {
     );
   }
 }
+*/
